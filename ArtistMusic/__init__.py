fdfcf99f9488c9b64e124592c693de594ab9d62f
Artist <satyamshiv55@gmail.com>
2026-05-07 18:57:45 +0530

    Update __init__.py

--- ArtistMusic/__init__.py
+++ ArtistMusic/__init__.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
-from AloneMusic.core.bot import Alone
-from AloneMusic.core.dir import dirr
-from AloneMusic.core.userbot import Userbot
-from AloneMusic.misc import dbb, heroku
+from Artist.core.bot import Artist
+from Artist.core.dir import dirr
+from Artist.core.userbot import Userbot
+from Artist.misc import dbb, heroku
 
 from .logging import LOGGER
 
@@ -18,7 +9,7 @@ dirr()
 dbb()
 heroku()
 
-app = Alone()
+app = Artist()
 userbot = Userbot()
 
 
