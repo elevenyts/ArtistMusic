848db6fe2a926ae00b78e1d0835b3ee702f59bb7
Artist <satyamshiv55@gmail.com>
2026-05-07 18:50:34 +0530

    Update start.py

--- ArtistMusic/utils/inline/start.py
+++ ArtistMusic/utils/inline/start.py
@@ -1,16 +1,7 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram.types import InlineKeyboardButton
 
 import config
-from AloneMusic import app
+from ArtistMusic import app
 
 
 def start_panel(_):
