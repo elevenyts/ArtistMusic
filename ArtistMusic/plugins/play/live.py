732cd301ebe1676670b27d5a9ec5e54c8573a77b
Artist <satyamshiv55@gmail.com>
2026-05-07 17:56:36 +0530

    Update live.py

--- ArtistMusic/plugins/play/live.py
+++ ArtistMusic/plugins/play/live.py
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import filters
 
-from AloneMusic import YouTube, app
-from AloneMusic.utils.channelplay import get_channeplayCB
-from AloneMusic.utils.decorators.language import languageCB
-from AloneMusic.utils.stream.stream import stream
+from ArtistMusic import YouTube, app
+from ArtistMusic.utils.channelplay import get_channeplayCB
+from ArtistMusic.utils.decorators.language import languageCB
+from ArtistMusic.utils.stream.stream import stream
 from config import BANNED_USERS
 
 
