1bf80cb48e3ee90fa8cf57da95e5df2ac0753bb2
Artist <satyamshiv55@gmail.com>
2026-05-07 18:34:06 +0530

    Update logger.py

--- ArtistMusic/utils/logger.py
+++ ArtistMusic/utils/logger.py
@@ -1,18 +1,9 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 import os
 
 from pyrogram.enums import ParseMode
 
-from AloneMusic import app
-from AloneMusic.utils.database import is_on_off
+from ArtistMusic import app
+from ArtistMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
 
