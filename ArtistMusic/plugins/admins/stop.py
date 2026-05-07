f76bc1e3bd705baceb6042f6b1a70d0aeddb6e9a
Artist <satyamshiv55@gmail.com>
2026-05-07 11:30:33 +0530

    Update stop.py

--- ArtistMusic/plugins/admins/stop.py
+++ ArtistMusic/plugins/admins/stop.py
@@ -1,20 +1,11 @@
-#
-# Copyright (C) 2021-2022 by TheAloneteam@Github, < https://github.com/TheAloneTeam >.
-#
-# This file is part of < https://github.com/TheAloneTeam/AloneMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AloneMusic import app
-from AloneMusic.core.call import Alone
-from AloneMusic.utils.database import set_loop
-from AloneMusic.utils.decorators import AdminRightsCheck
-from AloneMusic.utils.inline import close_markup
+from ArtistMusic import app
+from ArtistMusic.core.call import Artist
+from ArtistMusic.utils.database import set_loop
+from ArtistMusic.utils.decorators import AdminRightsCheck
+from ArtistMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -25,7 +16,7 @@ from config import BANNED_USERS
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return
-    await Alone.stop_stream(chat_id)
+    await Artist.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_5"].format(message.from_user.mention), reply_markup=close_markup(_)
